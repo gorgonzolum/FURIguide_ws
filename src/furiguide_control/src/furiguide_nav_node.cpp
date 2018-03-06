151ffc0c7ad40e25ad3737c79d8f956082188f61
gorgonzolum <gorgonzolum@gmail.com>
2018-03-05 18:38:23 -0700

    rudimentiary tag heading following

--- src/furiguide_control/src/furiguide_nav_node.cpp
+++ src/furiguide_control/src/furiguide_nav_node.cpp
@@ -1,6 +1,9 @@
 #include <ros/ros.h>
 #include <tf2_ros/transform_listener.h>
+#include <tf2/impl/utils.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/PoseWithCovariance.h>
 #include <geometry_msgs/Twist.h>
 #include <nav_msgs/Odometry.h>
 #include <apriltags_ros/AprilTagDetection.h>
@@ -10,26 +13,24 @@
 
 ros::Publisher tag_pose_pub;
 ros::Publisher steer_twist_pub;
-geometry_msgs::Pose current_pose;
+
+geometry_msgs::PoseWithCovariance current_pose;
 geometry_msgs::PoseStamped current_waypoint_pose;
+bool hasWaypoint = false;
 
+// Populated by the tf listener
 tf2_ros::Buffer tfBuffer;
-tf2_ros::TransformListener tfListener(tfBuffer);
-void processOdometry(const nav_msgs::Odometry& msg) {
-    current_pose = msg.pose.pose;
-
-    steerToWaypoint();
-}
 
 void processAprilTag(const apriltags_ros::AprilTagDetection& tag) {
   ROS_INFO("Processing Waypoint");
-  current_waypoint_pose = tag.pose;
 
   geometry_msgs::TransformStamped transformStamped;
 
   // look up the transform of the camera from tf2, then store the pose relative to the base_link
   try{
-    transformStamped = tfBuffer.lookupTransform("camera_link", "base_link", ros::Time(0));
+    transformStamped = tfBuffer.lookupTransform("odom", "camera_link_optical", ros::Time(0));
+    tf2::doTransform(tag.pose, current_waypoint_pose, transformStamped);
+    hasWaypoint = true;
   }
   catch (tf2::TransformException &ex) {
     ROS_WARN("%s", ex.what());
@@ -41,25 +42,49 @@ void steerToWaypoint() {
   geometry_msgs::Twist twist;
   
   ROS_INFO("Correcting robot angle to waypoint");
-  // transform pose camera to odom frame
-  // get tf from robot_state_publisher
-  // calculate new heading, orient toward apriltag pose with simple proportional controller or PID (or OTHER)
+  // TODO: use something more sophitisticated to correct and drive forward
+  
+  tf2::Quaternion odomQuat, waypointQuat;
+  tf2::fromMsg(current_pose.pose.orientation, odomQuat);
+  tf2::fromMsg(current_waypoint_pose.pose.orientation, waypointQuat);
+
+  double waypointYaw = tf2::impl::getYaw(waypointQuat);  
+  double odomYaw = tf2::impl::getYaw(odomQuat);  
+
+  //double dTheta = waypointQuat.angleShortestPath(odomQuat);
+  double dTheta = waypointYaw - odomYaw;
+  
+  ROS_INFO("Tag: %f, Odom: %f, Angle: %f", waypointYaw, odomYaw, dTheta);
+  twist.linear.x = 0.05; 
+  twist.angular.z = dTheta * 0.5;
 
   steer_twist_pub.publish(twist);
 }
 
+// Subscriber Callbacks
 void tagDetection_cb(const apriltags_ros::AprilTagDetectionArray& msg) {
   for (int i=0; i < msg.detections.size(); i++) {
     processAprilTag(msg.detections[i]);
   }
 }
 
+void odometry_cb(const nav_msgs::Odometry& msg) {
+    current_pose = msg.pose;
+
+    if (hasWaypoint)
+      steerToWaypoint();
+}
+
 int main(int argc, char *argv[]) {
   ros::init(argc, argv, "furiguide_nav_node");
+
   ros::NodeHandle nh;
-  ros::Subscriber sub = nh.subscribe("tag_detections", 10, tagDetection_cb);
+  ros::Subscriber tagSub = nh.subscribe("tag_detections", 10, tagDetection_cb);
+  ros::Subscriber odomSub = nh.subscribe("odom", 10, odometry_cb);
   tag_pose_pub = nh.advertise<geometry_msgs::PoseStamped>("tag_pose", 1);
   steer_twist_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+
+  tf2_ros::TransformListener tfListener(tfBuffer);
 
   ros::Rate r(10);
   while (ros::ok()) {

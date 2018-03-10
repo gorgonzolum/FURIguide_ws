db112be66428336d9b22a1e8d08d7b8540db92c0
gorgonzolum <gorgonzolum@gmail.com>
2018-03-10 11:10:20 -0700

    improved tag following with sequences and pointing

--- src/furiguide_control/src/furiguide_nav_node.cpp
+++ src/furiguide_control/src/furiguide_nav_node.cpp
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <math.h>
 #include <ros/ros.h>
 #include <tf2_ros/transform_listener.h>
 #include <tf2/impl/utils.h>
@@ -11,24 +13,50 @@
 
 #include "furiguide_nav_node.h"
 
+#define correctAngle(X, L) X > 0 ? X : X + L
+
 ros::Publisher tag_pose_pub;
 ros::Publisher steer_twist_pub;
 
 geometry_msgs::PoseWithCovariance current_pose;
 geometry_msgs::PoseStamped current_waypoint_pose;
+int current_waypoint_id = -1;
 bool hasWaypoint = false;
 
 // Populated by the tf listener
 tf2_ros::Buffer tfBuffer;
 
+double shortestAngle(double A, double B) {
+  double diff = A - B;
+
+  if (diff > M_PI)
+    return diff - 2*M_PI;
+  else if (diff < -M_PI)
+    return diff + 2*M_PI;
+
+  return diff;
+}
+
 void processAprilTag(const apriltags_ros::AprilTagDetection& tag) {
-  ROS_INFO("Processing Waypoint");
+  ROS_INFO("Processing Waypoint: %d", tag.id);
+
+  // ignore extraneous waypoints (avoid backtracking or seeing too far)
+  if (tag.id < current_waypoint_id || tag.id > current_waypoint_id + 1) {
+    return; // ignore this tag
+  }
+
+  // look for the next waypoint
+  if (tag.id == current_waypoint_id + 1) {
+    current_waypoint_id++;
+  }
 
   geometry_msgs::TransformStamped transformStamped;
 
   // look up the transform of the camera from tf2, then store the pose relative to the base_link
+  // be careful to get the transform at the time of tag detection for accurate positioning information
+  // this effect is especially bad in simulation when manually positioning the robot
   try{
-    transformStamped = tfBuffer.lookupTransform("odom", "camera_link_optical", ros::Time(0));
+    transformStamped = tfBuffer.lookupTransform("odom", "camera_link_optical", tag.pose.header.stamp);
     tf2::doTransform(tag.pose, current_waypoint_pose, transformStamped);
     hasWaypoint = true;
   }
@@ -41,18 +69,19 @@ void processAprilTag(const apriltags_ros::AprilTagDetection& tag) {
 void steerToWaypoint() {
   geometry_msgs::Twist twist;
   
-  ROS_INFO("Correcting robot angle to waypoint");
+  ROS_INFO("Correcting robot angle to waypoint %d", current_waypoint_id);
   // TODO: use something more sophitisticated to correct and drive forward
   
   tf2::Quaternion odomQuat, waypointQuat;
   tf2::fromMsg(current_pose.pose.orientation, odomQuat);
   tf2::fromMsg(current_waypoint_pose.pose.orientation, waypointQuat);
 
-  double waypointYaw = tf2::impl::getYaw(waypointQuat);  
-  double odomYaw = tf2::impl::getYaw(odomQuat);  
+  double waypointYaw = correctAngle(tf2::impl::getYaw(waypointQuat), 2*M_PI);
+  double odomYaw = correctAngle(tf2::impl::getYaw(odomQuat), 2*M_PI);
 
-  //double dTheta = waypointQuat.angleShortestPath(odomQuat);
-  double dTheta = waypointYaw - odomYaw;
+  double dPos = tf2::tf2Distance2(posVec, wayVec);
+  double dTheta = 0;
+  dTheta = -1 * shortestAngle(odomYaw, waypointYaw); // the differential drive controller in gazebo has negative as CW (???)
   
   ROS_INFO("Tag: %f, Odom: %f, Angle: %f", waypointYaw, odomYaw, dTheta);
   twist.linear.x = 0.05; 
